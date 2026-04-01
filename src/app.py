5e8325e40a9ebb735ea74785fc546ea37dac52b5
PSTNPX-PC\pstnpx <ps.tanupat@gmail.com>
2026-04-02 00:36:26 +0700

    Add main

--- src/app.py
+++ src/app.py
@@ -2,11 +2,16 @@ from libs.ClientIntervalApp import ClientIntervalApp
 import logging
 from config import LOG_FILENAME
 
-logger = logging.getLogger(LOG_FILENAME) 
-app = ClientIntervalApp()
-try:
-    app.run()
-except KeyboardInterrupt:
-    logger.info("-------- Collector app stopped by user ---------")
-except Exception as final_e:
-    logger.critical(f"Unhandled critical exception in main loop: {final_e}", exc_info=True)
+logger = logging.getLogger(LOG_FILENAME)
+
+def main():
+    app = ClientIntervalApp()
+    try:
+        app.run()
+    except KeyboardInterrupt:
+        logger.info("-------- Collector app stopped by user ---------")
+    except Exception as final_e:
+        logger.critical(f"Unhandled critical exception in main loop: {final_e}", exc_info=True)
+
+if __name__ == "__main__":
+    main()
